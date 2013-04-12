cac04be975fb48e06e1c022c162866c5351058e0
david viramontes <dviramontes@gmail.com>
2013-04-12 10:11:11 -0600

    validates markup pwith as validator w3, 1st pass

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,9 @@
+var position;
+
 $(document).ready(function(){
-	//  add pad left by x amount class
 	$('.media button').addClass("pad-left-by-x");
-
-	// $(window).scroll(function(e){
-	// 	console.dir(e);
-	// 	console.log(e.type);
-	// 	$('.container-fluid').css('height', '100px');
-	// });
-
 	$(window).scroll(function(){
-		var position = $('body').scrollTop();
-		$('#black-container').css('height', '150px');
-		$('#black-container >img').css("width", "300px");	
-		if(position !== 0 ) {
-			$('#black-container').css(
-				{
-					'height':'100px'
-
-				});
-			$('#black-container >img').css("width", "10%");
-		}
-	})
+		position = $('body').scrollTop();
+		console.log(position);
+	});
 });
