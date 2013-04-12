3ce1911b23f1f71a1067636a779a4847611cbe77
david viramontes <dviramontes@gmail.com>
2013-04-12 12:24:19 -0600

    finalizes scrolltop animation for magazine-logo

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,24 @@
-var position;
+
 
 $(document).ready(function(){
+
 	$('.media button').addClass("pad-left-by-x");
+
 	$(window).scroll(function(){
-		position = $('body').scrollTop();
-		console.log(position);
+
+		var position = $('body').scrollTop();
+
+		if(position !== 0) {
+			$(".magazine-logo").removeClass("magazine-logo-change");
+			$(".container-fluid").removeClass("container-fluid-change");
+		}else if(position ===0){
+			// $(".magazine-logo").toggleClass("magazine-logo-change");
+		}
+		$(".container-fluid").toggleClass("container-fluid-change");
+		$(".magazine-logo").toggleClass("magazine-logo-change");
+
 	});
+
+
 });
+
