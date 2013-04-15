c5b1adb88344b6482c15888611473ffd7c0a4f7c
david viramontes <dviramontes@gmail.com>
2013-04-14 22:36:25 -0600

    adds Adams Schwartz stylish console.log

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,9 @@ $(document).ready(function(){
 		$(".magazine-logo").removeClass("magazine-logo-change");
 		$(".container-fluid").removeClass("container-fluid-change");
 		$(".msudenver-logo").removeClass("msudenver-logo-change");
-		if(position > 35) {
+		console.log(position);
+
+		if(position >= 100) {
 			$(".container-fluid").toggleClass("container-fluid-change");
 			$(".magazine-logo").toggleClass("magazine-logo-change");
 			$(".msudenver-logo").toggleClass("msudenver-logo-change");
