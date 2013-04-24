0e24b3d6274b5490e550e3885543e9e7bfe72e5f
david viramontes <dviramontes@gmail.com>
2013-04-24 11:53:29 -0600

    cleans .magazine-preview css

--- public/js/main.js
+++ public/js/main.js
@@ -3,17 +3,16 @@ $(document).ready(function(){
 	'use strict' ;
 
 	var width_threshold = 1200;
-	var ulr_to_magazine= "http://google.com";
 
 	// remove for production
 	sizedebuggin();
-	
+
 	// makes magazine preview clickable 
 	$("section.magazine-preview").click(function(){
-		ulr_to_magazine = $(this).data().href ;
-		// document.location.href = "http://www.google.com";
-		window.open(ulr_to_magazine, '_blank');
+		var ulr = $(this).data().href ;
+		window.open(ulr, '_blank');
 	});
+
 	// scrollTop function
 	$(window).scroll(function(){
 		var position = $(window).scrollTop();
@@ -22,11 +21,7 @@ $(document).ready(function(){
 		$(".container-fluid").removeClass("container-fluid-change");
 		$(".msudenver-logo").removeClass("msudenver-logo-change");
 
-		// reset image path 
-		// changeImagePath("img/responsive/msud-logo-A.png");
-
 		if( position > 10 && width > width_threshold) {
-			// changeImagePath();
 			$(".container-fluid").toggleClass("container-fluid-change");
 			$(".magazine-logo").toggleClass("magazine-logo-change");
 			$(".msudenver-logo").toggleClass("msudenver-logo-change");
