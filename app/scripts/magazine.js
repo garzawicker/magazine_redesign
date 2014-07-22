5536073f57dc5cbb6fd32598a1eaad66f4730b24
Miguel Garza Wicker <garzawi@msudenver.edu>
2014-07-22 16:39:21 -0600

    adds magazine.js as the unique js for magazine

---
+++ app/scripts/magazine.js
@@ -0,0 +1,51 @@
+
+$(document).ready(function(){
+	'use strict' ;
+
+	var width_threshold = 1200;
+	var currentLocation =  (document.location.href).toString();
+
+	// removes sharing icons from full story template
+	magazineLocationTest();
+	// make thumbails clickable if location == main magainze site
+	patternMatchMagazineLocation();
+
+	function magazineLocationTest(){
+		var found = currentLocation.match(/magazine\/20*/g) || [];
+		if(found.length >= 1) {
+			$(".addthis").css("display","none") ;
+		}
+	}
+
+
+	function patternMatchMagazineLocation(){
+		var found = currentLocation.match(/magazine/g) || [];
+		if(found.length >= 1){
+			// makes magazine preview clickable
+			$(".magazine-preview, .thumbnail, .carousel-caption")
+				.click(function(){
+					var ulr = $(this).data().href ;
+					window.open(ulr,"_self");
+				});
+		}else{
+			// reset cursor if not on main magainze site
+			$('.thumbnail').css('cursor', 'inherit');
+		}
+	}
+
+	// scrollTop function
+	$(window).scroll(function(){
+		var position = $(window).scrollTop();
+		var width     =  $(window).width();
+		$(".magazine-logo").removeClass("magazine-logo-change");
+		$(".container-fluid").removeClass("container-fluid-change");
+		$(".msudenver-logo").removeClass("msudenver-logo-change");
+
+		if( position > 15 && width > width_threshold) {
+			$(".container-fluid").toggleClass("container-fluid-change");
+			$(".magazine-logo").toggleClass("magazine-logo-change");
+			$(".msudenver-logo").toggleClass("msudenver-logo-change");
+		}
+
+	});
+});
