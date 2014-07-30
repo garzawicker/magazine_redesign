9b5729db7d2b3b1d261b6a380045f5d3332f7bc3
Miguel Garza Wicker <garzawi@msudenver.edu>
2014-07-30 14:10:21 -0600

    removes unused js functions

--- app/scripts/magazine.js
+++ app/scripts/magazine.js
@@ -3,35 +3,17 @@ $(document).ready(function(){
 	'use strict' ;
 
 	var width_threshold = 900;
-	var currentLocation =  (document.location.href).toString();
-
-	// removes sharing icons from full story template
-	magazineLocationTest();
-	// make thumbails clickable if location == main magainze site
-	patternMatchMagazineLocation();
-
-	function magazineLocationTest(){
-		var found = currentLocation.match(/magazine\/20*/g) || [];
-		if(found.length >= 1) {
-			$(".addthis").css("display","none") ;
+	var href = document.location.href;
+	
+	// Turns elements with class .clickable into links
+	$(".clickable")
+		.click(function(event) {
+		if ($(this).data().href) {
+			//event.preventDefault()
+			return window.open($(this).data().href, "_self");
 		}
-	}
+	});
 
-
-	function patternMatchMagazineLocation(){
-		var found = currentLocation.match(/magazine/g) || [];
-		if(found.length >= 1){
-			// makes magazine preview clickable
-			$(".magazine-preview, .thumbnail, .carousel-caption")
-				.click(function(){
-					var ulr = $(this).data().href ;
-					window.open(ulr,"_self");
-				});
-		}else{
-			// reset cursor if not on main magainze site
-			$('.thumbnail').css('cursor', 'inherit');
-		}
-	}
 
 	// scrollTop function
 	$(window).scroll(function(){
